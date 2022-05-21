829eadd69569644dd8d776e271f3c07de3e62499
Coda Hale <coda.hale@gmail.com>
2022-05-21 13:29:12 -0600

    docs: add module docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+//! qdsa provides a small, portable implementation of the
+//! [qDSA](https://joostrenes.nl/publications/qdsa-eprint.pdf) digital signature algorithm
+//! instantiated with the Curve25519 elliptic curve, plus the X25519 key agreement algorithm.
 #![cfg_attr(not(feature = "std"), no_std)]
 #![forbid(unsafe_code)]
 
