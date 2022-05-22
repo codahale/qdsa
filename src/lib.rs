4968b32af7e758fb6b61af0c6c9d9fcbb91ef5f3
Coda Hale <coda.hale@gmail.com>
2022-05-22 06:57:48 -0600

    refactor(hazmat): move the dangerous stuff to a hazmat module

--- src/lib.rs
+++ src/lib.rs
@@ -4,15 +4,22 @@
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 
-pub use crate::point::{Point, G};
-pub use crate::qdsa::{sign, verify, verify_detached};
-pub use crate::scalar::Scalar;
+use crate::point::G;
+pub use crate::qdsa::{sign, verify};
+use crate::scalar::Scalar;
 pub use crate::x25519::x25519;
 
 mod point;
 mod qdsa;
 mod scalar;
 mod x25519;
+
+/// Cryptographic functionality which will let you do stupid things to yourself.
+pub mod hazmat {
+    pub use crate::point::{Point, G};
+    pub use crate::qdsa::verify_detached;
+    pub use crate::scalar::Scalar;
+}
 
 /// Given a secret key `sk`, returns the corresponding public key.
 pub fn public_key(sk: &[u8; 32]) -> [u8; 32] {
