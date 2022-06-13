374cb5726502ef54ce4152e7f59dfed9815c6321
Coda Hale <coda.hale@gmail.com>
2022-06-13 10:21:38 -0600

    refactor(elligator): add key_pair and rename fns

--- src/elligator.rs
+++ src/elligator.rs
@@ -1,4 +1,4 @@
-//! Elligator 2 maps Curve25519 points to uniform bitstrings and back.
+//! Elligator2 maps Curve25519 points to uniform bitstrings and back.
 
 use rand_core::{CryptoRng, RngCore};
 
@@ -6,7 +6,7 @@ use crate::hazmat::Point;
 
 /// Converts public key `pk` to an Elligator2 representative, if possible. Uses `rng` to mask the
 /// constant bits of the resulting Curve25519 field element. Not all points map to representatives.
-pub fn point_to_representative(
+pub fn public_key_to_representative(
     pk: &[u8; 32],
     mut rng: impl RngCore + CryptoRng,
 ) -> Option<[u8; 32]> {
@@ -18,6 +18,23 @@ pub fn point_to_representative(
 
 /// Converts Elligator2 representative `rep` to a Curve25519 public key. All possible
 /// representatives map to points.
-pub fn representative_to_point(rep: &[u8; 32]) -> [u8; 32] {
+pub fn representative_to_public_key(rep: &[u8; 32]) -> [u8; 32] {
     Point::from_elligator(rep).as_bytes()
+}
+
+/// Generates a random secret key, public key, and Elligator2 representative.
+pub fn key_pair(mut rng: impl RngCore + CryptoRng) -> ([u8; 32], [u8; 32], [u8; 32]) {
+    loop {
+        // Generate a secret key.
+        let mut sk = [0u8; 32];
+        rng.fill_bytes(&mut sk);
+
+        // Calculate the public key.
+        let pk = crate::public_key(&sk);
+
+        // Try to calculate the representative.
+        if let Some(rep) = public_key_to_representative(&pk, &mut rng) {
+            return (sk, pk, rep);
+        }
+    }
 }
